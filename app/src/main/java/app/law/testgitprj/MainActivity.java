330c16f7e38b2958877b2e90d64db52ea452a911
lawrencewithandroid <tobefree2233@gmail.com>
2017-03-24 02:39:05 +0800

    OK La !!!

--- app/src/main/java/app/law/testgitprj/MainActivity.java
+++ app/src/main/java/app/law/testgitprj/MainActivity.java
@@ -1,5 +1,6 @@
 package app.law.testgitprj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
             }
         });
 

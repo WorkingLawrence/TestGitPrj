6ce15d42ad911a779d9015eb6ebce036be8a3df7
lawrencewithandroid <tobefree2233@gmail.com>
2017-03-24 02:48:53 +0800

    New Branch DTO

---
+++ app/src/main/java/app/law/testgitprj/dto/TMember.java
@@ -0,0 +1,52 @@
+package app.law.testgitprj.dto;
+
+import java.io.Serializable;
+
+public class TMember implements Serializable
+{
+    public String getMemberId()
+    {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId)
+    {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName()
+    {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName)
+    {
+        this.memberName = memberName;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    public String getPhoneNum()
+    {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum)
+    {
+        this.phoneNum = phoneNum;
+    }
+
+    private String memberId;
+    private String memberName;
+    private String gender;
+    private String phoneNum;
+
+}

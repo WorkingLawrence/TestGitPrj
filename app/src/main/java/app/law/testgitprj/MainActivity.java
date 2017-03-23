63202005001a8f3500493f24f7576209e7d3af52
lawrencewithandroid <tobefree2233@gmail.com>
2017-03-24 02:30:25 +0800

    Commit 2

--- app/src/main/java/app/law/testgitprj/MainActivity.java
+++ app/src/main/java/app/law/testgitprj/MainActivity.java
@@ -2,6 +2,8 @@ package app.law.testgitprj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -11,7 +13,17 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainlayout);
         Toast.makeText(this, "Hello World !!", Toast.LENGTH_SHORT).show();
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+
     }
 }
